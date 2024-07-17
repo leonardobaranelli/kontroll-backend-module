a911f1ea3ac386a8847b2884a76c6e50b96111a6
leo <leobaranelli@gmail.com>
2024-07-17 18:05:34 -0300

    refactor(handleStep): complete process response (again)

--- src/services/carrier.service.ts
+++ src/services/carrier.service.ts
@@ -72,18 +72,20 @@ export default class CarrierService {
           message: `Process completed successfully! Carrier ${state.name} created.`,
           nextStep: '',
           stepsDetails: {
-            step: '',
-            stepTitle: '',
-            details1: '',
+            step: '5',
+            stepTitle: 'We got it!',
+            details1:
+              "You are now ready to start making requests and integrating with your application. If you encounter any issues, don't hesitate to refer back to the tutorial or reach out to support.",
             details2: '',
             details3: '',
             details4: '',
           },
           form: {
             expectedFieldName: '',
-            instruction: '',
+            instruction:
+              'You have the option to go to the dashboard to review existing information or create a new connector to add data.',
             label: '',
-            title: '',
+            title: 'Your connection has been successfully set up!',
             placeholder: '',
           },
         };
