4b8c5f5478655443a99e6f36fdb594c3116cf6d7
leo <leobaranelli@gmail.com>
2024-07-22 16:20:21 -0300

    feat(carriers): couple automated system to get requirements via documentation step by step to integrate a carrier

--- src/core/carriers/get-req-via-doc/index.ts
+++ src/core/carriers/get-req-via-doc/index.ts
@@ -3,50 +3,41 @@ import { filterLinks } from './2-filter-scraped-site/filter-links';
 import { scrapeLinks } from './3-scrape-links/scrape-links';
 import { filterLinks2 } from './4a-filter-scraped-links/filter-links2';
 import { extractContent } from './4b-process-links/1-extract-content-from-scraped-links/extract-content';
-import { processContent } from './4b-process-links/2-process-content/process-content';
+import { extractSteps } from './4b-process-links/2-process-content/extract-steps';
 import { convergeSteps } from './5-converge-steps/converge-steps';
 
 export const getReqViaDoc = async (
   serviceName: string,
-  _linksKeywords: string[],
-  _contentKeywords: string[],
+  linksKeywords: string[],
+  contentKeywords: string[],
 ) => {
   try {
+    console.log('\nStarting site scraping...');
     await scrapeSite(serviceName);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
+    console.log('\nFiltering links from scraped site...');
     await filterLinks(serviceName, linksKeywords);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
-    await scrapeLinks(serviceName, contentKeywords);
+    console.log('\nScraping links for content...');
+    await scrapeLinks(serviceName, contentKeywords, 10);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
     await filterLinks2(serviceName, linksKeywords);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
+    console.log('\nExtracting content from scraped links...');
     await extractContent(serviceName);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
-    await processContent(serviceName);
+    console.log('\nExtracting steps from content...');
+    await extractSteps(serviceName);
     await new Promise((resolve) => setTimeout(resolve, 200));
 
+    console.log('\nConverging steps...');
     await convergeSteps(serviceName);
-    await new Promise((resolve) => setTimeout(resolve, 200));
-
-    console.log('\nAll secuence executed successfully');
   } catch (error) {
     console.error('Error in execution sequence:', error);
   }
 };
-
-const serviceName = 'DHL Global Forwarding';
-const linksKeywords = ['forNowJustEmptyWithTheServiceNameOnly'];
-const contentKeywords = [
-  'api key',
-  'auth',
-  'account number',
-  'client number',
-  'credential',
-];
-
-getReqViaDoc(serviceName, linksKeywords, contentKeywords);
--- src/services/helpers/carrier/get-steps.helper.ts
+++ src/services/helpers/carrier/get-steps.helper.ts
@@ -1,10 +1,19 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { getReqViaDoc } from '../../../core/carriers/get-req-via-doc';
 
 // Load JSON content from a file
+
 const loadJSON = (filePath: string): any => {
-  const rawData = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(rawData);
+  try {
+    const rawData = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(rawData);
+  } catch (error: any) {
+    if (error.code === 'ENOENT') {
+      return 'Data not found on the storage';
+    }
+    throw error;
+  }
 };
 
 // Function to count the number of objects in an array
@@ -16,21 +25,34 @@ const countObjectsInArray = (array: any[]): number => {
 };
 
 // Main
-export const carrierStepsData = (carrierName: string) => {
+export const carrierStepsData = async (carrierName: string) => {
   const formattedCarrierName = carrierName.toLowerCase().replace(/\s+/g, '_');
   const carrierFilePath = path.join(
     `./src/storage/carriers/data-on-steps/${formattedCarrierName}.json`,
   );
 
   try {
-    const dataOnSteps = loadJSON(carrierFilePath);
-
-    const numberOfSteps = countObjectsInArray(dataOnSteps);
-    console.log(`Number of steps: ${numberOfSteps}`);
-
-    return { dataOnSteps, numberOfSteps };
+    const dataOnSteps = await loadJSON(carrierFilePath);
+    if (dataOnSteps === 'Data not found on the storage') {
+      await getReqViaDoc(carrierName, linksKeywords, contentKeywords);
+      const dataOnSteps = await loadJSON(carrierFilePath);
+      const numberOfSteps = await countObjectsInArray(dataOnSteps);
+      return { dataOnSteps, numberOfSteps };
+    } else {
+      const numberOfSteps = await countObjectsInArray(dataOnSteps);
+      return { dataOnSteps, numberOfSteps };
+    }
   } catch (error) {
     console.error(error);
     throw new Error(`Error loading service configuration for ${carrierName}`);
   }
 };
+
+const linksKeywords = ['forNowJustEmptyWithTheServiceNameOnly'];
+const contentKeywords = [
+  'api key',
+  'auth',
+  'account number',
+  'client number',
+  'credential',
+];
