ecc60fd3fa4da1eeea45db4bbe784261a0e37bf6
leo <leobaranelli@gmail.com>
2024-08-07 17:36:06 -0300

    feat(firestore): clean data before saving to db

--- src/config/database/firestore/firestore.config.ts
+++ src/config/database/firestore/firestore.config.ts
@@ -21,55 +21,15 @@ async function verifyFirestoreConnection(): Promise<void> {
   }
 }
 
-function getConnectorsCollection() {
-  return admin.firestore().collection('connectors');
-}
-
-function getCarriersCollection() {
-  return admin.firestore().collection('carriers');
-}
-
-export {
-  initializeFirestore,
-  verifyFirestoreConnection,
-  getConnectorsCollection,
-  getCarriersCollection,
-};
-
-/*
-import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
-
-function initializeFirestore() {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
-}
-
-async function verifyFirestoreConnection(): Promise<void> {
-  try {
-    await admin
-      .firestore()
-      .collection('test')
-      .doc('testDoc')
-      .set({ test: 'testValue' });
-    console.log('Connection to Firestore has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to Firestore:', error);
-    throw error;
-  }
-}
-
-function getConnectorsCollection() {
-  return admin.firestore().collection('connectors');
-}
-
 function getCarriersCollection() {
   return admin.firestore().collection('carriers');
 }
 
 function cleanData(data: any) {
-  return JSON.parse(JSON.stringify(data));
+  const cleanedData = JSON.parse(
+    JSON.stringify(data, (_key, value) => (value === undefined ? null : value)),
+  );
+  return cleanedData;
 }
 
 async function addDocument(collectionName: string, data: any) {
@@ -85,9 +45,7 @@ async function addDocument(collectionName: string, data: any) {
 export {
   initializeFirestore,
   verifyFirestoreConnection,
-  getConnectorsCollection,
   getCarriersCollection,
+  cleanData,
   addDocument,
 };
-
-*/
